b5c05bc8473f6f77b3909d8fd8e0f154630fb491
TITAN <52470545+TITAN-code@users.noreply.github.com>
2021-07-12 17:54:50 -0400

    update args

--- regression_e2_sn2/utils/args.py
+++ regression_e2_sn2/utils/args.py
@@ -19,28 +19,28 @@ def parse_args(cross_val=False):
     parser.add_argument('-f', '--feature', default=50, type=int,
                         help='feature size for GNN')
     parser.add_argument('-d', '--depth', default=4, type=int,
-                        help='Number of steps for GNN')
+                        help='Number of layers in the WLN')
     parser.add_argument('-w', '--workers', default=10, type=int,
                         help='Number of workers')
     parser.add_argument('--model_dir', default='trained_model',
                         help='path to the checkpoint file of the trained model')
-    parser.add_argument('--desc_path', default=None,
-                        help='path to the file storing the descriptors (must be provided when using QM_GNN model)')
-    parser.add_argument('--data_path', default='data/regio_nonstereo_12k_QM', type=str,
+    parser.add_argument('--data_path', default='datasets/e2_sn2_regression.csv', type=str,
                         help='Path to reaction data')
     parser.add_argument('--ini_lr', default=0.001, type=float,
                         help='Initial learning rate')
     parser.add_argument('--lr_ratio', default=0.95, type=float,
                         help='Learning rate decaying ratio')
     parser.add_argument('--selec_batch_size', default=10, type=int,
-                        help='Batch size while training the selectivity model')
+                        help='Batch size while training the model')
     parser.add_argument('--selec_epochs', default=50, type=int,
-                        help='Number of epochs while training the selectivity model')
+                        help='Number of epochs while training the model')
     parser.add_argument('--splits', nargs=3, type=int, default=[10, 10, 80],
                         help='Split of the dataset into testing, validating, and training. The sum should be 100')
     parser.add_argument('--select_descriptors', nargs='+',
                         default=["partial_charge", "fukui_elec", "fukui_neu", "nmr"],
-                        help='(Optional) Selection of descriptors to feed to the ml_QM_GNN model')
+                        help='(Optional) Selection of descriptors to feed to the ml_QM_GNN model '
+                             '(either the default selection, "partial_charge nmr", "fukui_elec fukui_neu" '
+                             'or "only_bonds") ')
 
     if cross_val:
         parser.add_argument('--k_fold', default=10, type=int,
